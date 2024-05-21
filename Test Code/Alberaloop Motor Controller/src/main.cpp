bb89056839b7efd64062a0fecb9a39543ffb859d
MFalsetti <matteof@telus.net>
2024-05-21 13:28:47 -0600

    Started motor controller code

--- Test Code/Alberaloop Motor Controller/src/main.cpp
+++ Test Code/Alberaloop Motor Controller/src/main.cpp
@@ -1,43 +1,58 @@
-/**************************************************************************/
-/*! 
-    @file     trianglewave.pde
-    @author   Adafruit Industries
-    @license  BSD (see license.txt)
-
-    This example will generate a triangle wave with the MCP4725 DAC.   
-
-    This is an example sketch for the Adafruit MCP4725 breakout board
-    ----> http://www.adafruit.com/products/935
- 
-    Adafruit invests time and resources providing this open source code, 
-    please support Adafruit and open-source hardware by purchasing 
-    products from Adafruit!
-*/
-/**************************************************************************/
-#include <Wire.h>
 #include <Adafruit_MCP4725.h>
+#include <FlexCAN_T4.h>
+#include <Wire.h>
 
+// The DAC for voltage output
 Adafruit_MCP4725 dac;
 
+// The CAN interface
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can;
+
+void SetMotorSpeed(int speed, bool direction){
+
+}
+
+
 void setup(void) {
-  Serial.begin(115200);
-  Serial.println("Hello!");
+    // Start the serial terminal
+    Serial.begin(115200);
+    Serial.println("Welcome to Motor Control!");
 
-  delay(10000);
+    // Delay by 5 seconds for startup
+    delay(5000);
 
-  
+    // Start the CAN interface
+    can.begin();
+    can.setBaudRate(500000);  // 500 kbps
 
-  // For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
-  // For MCP4725A0 the address is 0x60 or 0x61
-  // For MCP4725A2 the address is 0x64 or 0x65
-  if (dac.begin(0x62, &Wire)){
-    Serial.println("DAC Ready!");
-  } else {
-    Serial.println("DAC unknown error");
-  
-  };
-    
-  Serial.println("Generating a triangle wave");
+    // Set all mailboxes to reject all messages
+    can.setMBFilter(REJECT_ALL);
+
+    // Enable all mailboxes to be interrupt enabled
+    can.enableMBInterrupts();
+
+    // Set mailbox zero to recieve only messages with ID 0x123
+    can.setMBFilter(MB0, 0x123);
+    // Can0.setMBUserFilter(MB0, 0x123, 0x7FF); // Last parameter is the mask. This is ANDed with the ID to determine if the message is accepted
+
+    // Set the function to run when a message is recieved
+    can.onReceive(MB0, canSniff);
+
+    Serial.begin(115200);
+    Serial.println("Hello!");
+
+    delay(10000);
+
+    // For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
+    // For MCP4725A0 the address is 0x60 or 0x61
+    // For MCP4725A2 the address is 0x64 or 0x65
+    if (dac.begin(0x62, &Wire)) {
+        Serial.println("DAC Ready!");
+    } else {
+        Serial.println("DAC unknown error");
+    };
+
+    Serial.println("Generating a triangle wave");
 }
 
 // Assuming 5V input
@@ -76,8 +91,4 @@ void loop(void) {
 
     // Delay by 5 seconds
     delay(5000);
-
-
-
-
 }
